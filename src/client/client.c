c6ded07dd03f4e544d1882e222dbff005affe27b
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2022-01-12 10:44:31 -0500

    Cleared out files; added header files

--- src/client/client.c
+++ src/client/client.c
@@ -1,43 +1,13 @@
-#include "pipe_networking.h"
-
-
-static void sighandler(int signo);
-
-int sd = -1;
-
+#include "client.h"
 
 int main() {
-  signal(SIGINT, sighandler);
+	initscr();
 
-  int server;
+	printw("Hello world!");
+	refresh();
 
-  server = client_handshake();
-  sd = server;
+	getch();
+	endwin();
 
-  char input[500];
-
-  while (1) {
-    printf(">>> ");
-
-    fgets(input, sizeof(input), stdin);
-    if (input[0] == '\n') continue;
-
-    input[strlen(input) - 1] = 0;
-    write(server, input, strlen(input));
-
-    int r = read(server, input, sizeof(input));
-    input[r] = 0;
-
-    printf("%s\n", input);
-  }
-}
-
-
-static void sighandler(int signo) {
-  if (signo == SIGINT) {
-    close(sd);
-
-    printf("\n");
-    exit(EXIT_SUCCESS);
-  }
+	return 0;
 }
