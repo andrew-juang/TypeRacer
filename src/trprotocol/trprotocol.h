1b519b1d6fee23495016564069ac1145b9e7050f
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2022-01-12 20:41:00 -0500

    Updated TRPacket Prototype

--- src/trprotocol/trprotocol.h
+++ src/trprotocol/trprotocol.h
@@ -1,9 +1,34 @@
 #ifndef TRPROTOCOL_H
 #define TRPROTOCOL_H
 
+// Requires C11 or higher support
 struct TRPacket {
     unsigned int type;
-    void *data;
+
+    union {
+        // Packet types 0 and 1
+        struct {
+            unsigned int uname_length;
+            char *username;
+        };
+
+        // Packet type 2
+        struct {
+            unsigned int text_length;
+            char *text;
+        };
+
+        // Packet type 3
+        unsigned int countdown;
+
+        // Packet type 5
+        struct {
+            unsigned int puname_length;
+            char *prog_username;
+            unsigned int progress;
+            unsigned int wpm;
+        };
+    };
 };
 
 // Functions:
