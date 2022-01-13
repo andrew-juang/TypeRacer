cf5af6ea40a5af5ced28dacf044909f80720527a
Andrew <andrewjuang01@gmail.com>
2022-01-12 22:08:14 -0500

    started server

--- src/server/server.c
+++ src/server/server.c
@@ -1,5 +1,47 @@
 #include "server.h"
 
 int main() {
-	return 0;
+	// Binds to a port and listens for incoming connections
+	int sd = server_setup();
+
+	while (1) {
+		int to_client = server_connect(sd);
+		printf("[server] connected to client!\n");
+	}
+}
+
+
+int server_setup() {
+	printf("[server] creating socket\n");
+	struct addrinfo *hints, *results;
+	hints = calloc(1, sizeof(struct addrinfo));
+	hints->ai_family = AF_INET;
+	hints->ai_socktype = SOCK_STREAM;
+	hints->ai_flags = AI_PASSIVE;
+	getaddrinfo("localhost", "9001", hints, &results);
+
+	int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	int bresult = bind(sd, results->ai_addr, results->ai_addrlen);
+	if (bresult) {
+		printf("[server] couldn't bind to socket\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("[server] created and bound to socket\n");
+
+	listen(sd, 10);
+	printf("[server] now listening on socket\n");
+
+	return sd;
+}
+
+int server_connect(int from_client) {
+	int client_socket;
+	socklen_t sock_size;
+	struct sockaddr_storage client_address;
+	sock_size = sizeof(client_address);
+
+	client_socket = accept(from_client, (struct sockaddr *) &client_address, &sock_size);
+
+	return client_socket;
 }
--- src/server/server.h
+++ src/server/server.h
@@ -1,4 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #ifndef SERVER_H
 #define SERVER_H
+
+int server_setup();
+int server_connect(int from_client);
+
+char * generate_text();
 
 #endif
