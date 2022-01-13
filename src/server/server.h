e6195d13e80ed7f2fab8f1bcca6e332d9bc432fb
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2022-01-13 13:56:12 -0500

    Fixed problems in server.c

--- src/server/server.h
+++ src/server/server.h
@@ -1,5 +1,9 @@
+#ifndef SERVER_H
+#define SERVER_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -11,9 +15,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
-#ifndef SERVER_H
-#define SERVER_H
 
 int server_setup();
 int server_connect(int from_client);
